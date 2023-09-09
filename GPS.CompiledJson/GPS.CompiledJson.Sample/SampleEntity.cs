b939b369816371fdaa3236a69a41367323ac9f78
sharpninja <ninja@thesharp.ninja>
2023-09-09 13:56:23 -0500

    Initial commit

---
+++ GPS.CompiledJson/GPS.CompiledJson.Sample/SampleEntity.cs
@@ -0,0 +1,10 @@
+using Generators;
+
+namespace GPS.CompiledJson.Sample;
+
+// This code will not compile until you build the project with the Source Generators
+
+[CompiledJson]
+public partial record SampleEntity
+{
+}
